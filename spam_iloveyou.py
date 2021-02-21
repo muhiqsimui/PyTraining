d5c704887acc20e1de7e32692d88fdaf3e60078e
Muhammad Iqbal <33776809+muhiqsimui@users.noreply.github.com>
2021-02-21 16:51:02 +0700

    Spamming I love you
    spamming text "i love you" using pyautogui

---
+++ spam_iloveyou.py
@@ -0,0 +1,22 @@
+import pyautogui as piu
+import time
+
+
+
+pesan=['I','Love','You']
+n=10
+
+print('Ready')
+
+waktu=5
+while (waktu !=0):
+	time.sleep(1)
+	waktu-=1
+
+for i in range(1,int(n)):
+	time.sleep(1)
+	for b in range (3):
+		piu.typewrite(pesan[b]+'\n')
+
+		
+		
