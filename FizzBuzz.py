021bd50721f6cbe4da0d7a1709c66977e4152fec
Muhammad Iqbal <33776809+muhiqsimui@users.noreply.github.com>
2021-04-27 17:25:44 +0700

    Update and rename fishbuzz.py to FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,15 +1,15 @@
-#CONTOH FISH BUZZ sederhana
+#CONTOH FIZZ BUZZ sederhana
 
-# jika bisa dibagi 3 dan 5 maka fish BUZZ
-# jika bisa dibagi 3 fish
+# jika bisa dibagi 3 dan 5 maka fizz BUZZ
+# jika bisa dibagi 3 fizz
 # jika bisa dibagi 5 buzz
 
 def fb(nilai):
 	if(nilai%3==0):
 		if(nilai%5==0):
-			print (str(nilai)+" is FISH_BUZZ")
+			print (str(nilai)+" is FIZZ_BUZZ")
 		else:
-			print(str(nilai)+" is FISH")
+			print(str(nilai)+" is FIZZ")
 	elif(nilai%5==0):
 		print(str(nilai)+" is BUZZ")
 	else:
