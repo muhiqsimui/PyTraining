7291c7db7651684b24752bc9a927bc532da9f4b8
Muhammad Iqbal <33776809+muhiqsimui@users.noreply.github.com>
2021-04-29 11:22:28 +0700

    Update halu_oop.py

--- trash/halu_oop.py
+++ trash/halu_oop.py
@@ -6,7 +6,7 @@ class User:
   def upgradeTipe(self,tipe):
     self.tipe=tipe
 
-user1=User("Iqbal","Programmer")
+user1=User("Iqbal","Prince")
 user2=User("Dita Karang","Idol")
 user3=User("Tzuyu","Singer")
 user4=[User("Joy","RV member"),User("Irene","RV member"),User("Wendy","RV Member")]
@@ -21,4 +21,16 @@ print(user1.name+" Have "+str(len(user4))+" bias then ")
 bias()
 
 user4[2].upgradeTipe("Girlfriend")
-print(user1.name+" <3 "+user4[2].name+" Cause she is my "+user4[2].tipe)
+
+tipeA=user4[2].tipe
+print(user1.name+" <3 "+user4[2].name+" Cause she is my "+tipeA)
+
+
+
+if(tipeA=="Girlfriend"):
+  condition=True
+else:
+  condition=False
+
+x="Lovely" if condition else "good"
+print(x)
