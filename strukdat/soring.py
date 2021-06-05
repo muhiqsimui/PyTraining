1873d640c58694b8764c316fce15b4c54fbb66e3
Muhammad Iqbal <33776809+muhiqsimui@users.noreply.github.com>
2021-06-05 19:58:23 +0700

    Create soring.py

---
+++ strukdat/soring.py
@@ -0,0 +1,14 @@
+def bubble_sort(array):
+    n = len(array) # jumlah list
+    # perulangan pertama
+    for i in range(n): 
+        # perulangan kedua
+        for j in range(n - i - 1):
+            # bandingkan masing" elemen
+            if array[j] > array[j + 1]:
+                # jika lebih besar, tukar.
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+arrey=[2,4,5,6,8,3,4]
+bubble_sort(arrey)
