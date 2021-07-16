e75571580205721e3398c2f6ec5469d6cd09a668
Muhammad Iqbal <33776809+muhiqsimui@users.noreply.github.com>
2021-07-16 19:06:05 +0700

    Create tulisanbalik.py

---
+++ tulisanbalik.py
@@ -0,0 +1,14 @@
+# TULISAN nya digituin pokoknya gk tau istilah nya ngasal aja
+
+def rev(txt):
+  g=int(len(txt)/2)
+  print(txt[g:]+txt[:g])
+  # if len(txt)%2==0:
+  #   print(txt[g:]+txt[:g])
+  # else:
+  #   print('eror pie iki')
+
+tulisan='sabi lika taki jarbela honpyt engbar'
+les =tulisan.split()
+for i in range(len(les)):
+  rev(les[i])
